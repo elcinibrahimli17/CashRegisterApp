6f5e362085ab31351fab58d534ceac7d5dd35dd3
Elcin <elcinibrahimli17@gmail.com>
2026-04-13 18:34:17 +0400

    Initial Commit

---
+++ CashRegisterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CashRegisterApp
+//
+//  Created by Elchın on 13.04.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
